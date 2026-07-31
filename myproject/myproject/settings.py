1c9eed00027e16ced9171506808f079e24291d0f
asadbek-git <asadmaqsudov999@gmail.com>
2026-07-31 10:09:47 +0500

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#iuwdb55^hx4!fzrdi&o5+p@#x#5%y%s5^desj*f787n=5=!s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello',
 ]
 
 MIDDLEWARE = [
